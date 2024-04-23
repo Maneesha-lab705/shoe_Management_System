e19eee5d20107d4bf1a6d0ef3fbe7bf4f4f3525d
Maneesha-lab705 <124575388+Maneesha-lab705@users.noreply.github.com>
2024-04-23 16:05:29 +0530

    init the project

---
+++ src/main/java/lk/ijse/shoemanagementsystem/ShoeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.shoemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/shoemanagementsystem/ShoeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.shoemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
